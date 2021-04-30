6040e90a8fb8db3db29ca72008ca1b254d2b141f
Aaron Trujillo <atrujilloembry@gmail.com>
2021-04-29 20:55:30 -0400

    se agrega comunas

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_cors import CORS
-from models import db, Servicios, Perfil
+from models import db, Servicios, Perfil, Solicitudes, Comunas
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
@@ -59,6 +59,7 @@ def get_perfil():
         perfil.Telefono = request.json.get("Telefono")
         perfil.Direccion = request.json.get("Direccion")
         perfil.Pregunta = request.json.get("Pregunta")
+        perfil.Respuesta = request.json.get("Respuesta")
         perfil.Id_Comuna = request.json.get("Id_Comuna")
         perfil.Comunas_Atencion = request.json.get("Comunas_Atencion")
         perfil.Experiencia = request.json.get("Experiencia")
@@ -72,6 +73,51 @@ def get_perfil():
         perfiles = list(map(lambda perfil: perfil.serialize_strict(), perfiles))
         return jsonify(perfiles), 200
 
+
+@app.route("/solicitudes", methods=["GET", "POST"])
+def get_solicitudes():
+    if request.method == "POST":
+        solicitudes = Solicitudes()
+        solicitudes.Id_Solicitud = request.json.get("Id_Solicitud")
+        solicitudes.Id_Servicio = request.json.get("Id_Servicio")
+        solicitudes.Id_Perfil = request.json.get("Id_Perfil")
+        solicitudes.id_Comuna = request.json.get("id_Comuna")
+        solicitudes.Estado_Solicitud = request.json.get("Estado_Solicitud")        
+        solicitudes.Nombres = request.json.get("Nombres")
+        solicitudes.Apellidos = request.json.get("Apellidos")
+        solicitudes.Telefono_Contacto = request.json.get("Telefono_Contacto")
+        solicitudes.Direccion = request.json.get("Direccion")
+        solicitudes.Calificacion = request.json.get("Calificacion")
+        solicitudes.Fecha = request.json.get("Fecha")
+        solicitudes.Hora = request.json.get("Hora")
+
+        db.session.add(solicitudes)
+        db.session.commit()
+        return jsonify(solicitudes.serialize_all_fields()), 200
+
+    if request.method == "GET":
+        solicitud = Solicitudes.query.all()
+        solicitud = list(map(lambda solicitud: solicitud.serialize_strict(), solicitudes))
+        return jsonify(solicitudes), 200
+
+
+@app.route("/comunas", methods=["GET", "POST"])
+def get_comunas():
+    if request.method == "POST":
+        comunas = Comunas()
+        comunas.Id_Region = request.json.get("Id_Region")
+        comunas.Id_Comuna = request.json.get("Id_Comuna")
+        comunas.Nombre_Comuna = request.json.get("Nombre_Comuna")
+        db.session.add(comunas)
+        db.session.commit()
+        return jsonify(comunas.serialize_all_fields()), 200
+
+    if request.method == "GET":
+        comuna = Comunas.query.all()
+        comuna = list(map(lambda comuna: comuna.serialize_strict(), comunas))
+        return jsonify(comunas), 200
+
+        
 
 if __name__ == "__main__":
     manager.run()
