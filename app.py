fe2221cfaa2c28b94a9b5e89fb9a02cc791f1b1a
Julio <juliomederosarias@gmail.com>
2021-05-05 12:05:15 -0400

    completando relaciones de models

--- app.py
+++ app.py
@@ -4,16 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_cors import CORS
-from models import db, Services, Profile, Communes, User, Requests, Availability
+from models import db, Services, Profile, Communes, User
 from flask_bcrypt import Bcrypt
-from datetime import date, datetime, time
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASEDIR, 'te_ayudo.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASEDIR, 'test.db')
 app.config["DEBUG"] = True
 app.config["ENV"] = "development"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -86,7 +85,6 @@ def get_profile_id(id):
 
             db.session.commit()
             return jsonify("Profile updated"), 200
-            
 @app.route('/user/profile', methods=["GET", "POST"])
 def get_profile():
     if request.method == "POST":
@@ -130,8 +128,8 @@ def get_profile():
 def get_services():
     if request.method == "POST":
         service = Services()
-        service.id_service = request.json.get("id_service")
-        service.name_service = request.json.get("name_service")
+        service.Id_Service = request.json.get("Id_Service")
+        service.Name_Service = request.json.get("Name_Service")
         db.session.add(service)
         db.session.commit()
         return jsonify(service.serialize_all_fields()), 200
@@ -141,14 +139,13 @@ def get_services():
         services = list(map(lambda service: service.serialize_strict(), services))
         return jsonify(services), 200
 
-
 @app.route("/communes", methods=["GET", "POST"])
 def get_communes():
     if request.method == "POST":
         communes = Communes()
-        communes.name_region = request.json.get("name_region")
-        communes.id_commune = request.json.get("id_commune")
-        communes.name_commune = request.json.get("name_commune")
+        communes.Id_Region = request.json.get("Id_Region")
+        communes.Id_Commune = request.json.get("Id_Commune")
+        communes.Name_Commune = request.json.get("Name_Commune")
         db.session.add(communes)
         db.session.commit()
         return jsonify(communes.serialize_all_fields()), 200
@@ -157,84 +154,6 @@ def get_communes():
         communes = Communes.query.all()
         communes = list(map(lambda commune: commune.serialize_strict(), communes))
         return jsonify(communes), 200
-
-@app.route("/availability", methods=["GET", "POST"])
-def get_availability():
-    if request.method == "POST":
-        availability = Availability()
-        availability.id_profile = request.json.get("id_profile")             
-        availability.date = request.json.get("date")
-        availability.hour = request.json.get("hour")
-        db.session.add(availability)
-        db.session.commit()
-        return jsonify(availability.serialize_all_fields()), 200
-
-    if request.method == "GET":
-        availabilitys = Availability.query.all()
-        availabilitys = list(map(lambda availability: availability.serialize_strict(), availabilitys))
-        return jsonify(availabilitys), 200
-
-@app.route("/ratings", methods=["GET", "POST"])
-def get_ratings():
-    if request.method == "POST":
-        rating = Ratings()
-        rating.id_profile = request.json.get("id_profile")             
-        rating.rating = request.json.get("rating")
-        db.session.add(rating)
-        db.session.commit()
-        return jsonify(rating.serialize_all_fields()), 200
-
-    if request.method == "GET":
-        ratings = Ratings.query.all()
-        ratings = list(map(lambda rating: rating.serialize_strict(), ratings))
-        return jsonify(ratings), 200
-
-@app.route("/user", methods=["GET", "POST"])
-def get_user():
-    if request.method == "POST":
-        user = User()
-        user.id_user = request.json.get("id_user")             
-        user.rut = request.json.get("rut")
-        user.email = request.json.get("email")             
-        password_hash = bcrypt.generate_password_hash(request.json.get('password'))
-        user.password = password_hash
-        password_hash_prev = bcrypt.generate_password_hash(request.json.get('password_previous'))
-        user.password_previous = password_hash_prev     
-        db.session.add(user)
-        db.session.commit()
-        return jsonify(user.serialize_all_fields()), 200
-
-    if request.method == "GET":
-        users = User.query.all()
-        users = list(map(lambda user: user.serialize_strict(), users))
-        return jsonify(users), 200
-
-@app.route("/requests", methods=["GET", "POST"])
-def get_requests():
-    if request.method == "POST":
-        requests = Requests()
-        requests.id_request = request.json.get("id_request")
-        requests.id_service = request.json.get("id_service")
-        requests.id_profile = request.json.get("id_profile")
-        requests.id_commune = request.json.get("id_commune")
-        requests.request_status = request.json.get("request_status")
-        requests.full_name = request.json.get("full_name")
-        requests.last_name = request.json.get("last_name")
-        requests.contact_phone = request.json.get("contact_phone")
-        requests.address = request.json.get("address")
-        requests.rating = request.json.get("rating")
-        requests.date = request.json.get("date")
-        requests.hour = request.json.get("hour")
-
-        db.session.add(requests)
-        db.session.commit()
-        return jsonify(requests.serialize_all_fields()), 200
-
-    if request.method == "GET":
-        requests = Requests.query.all()
-        requests = list(map(lambda request: request.serialize_strict(), requests))
-        return jsonify(requests), 200
-
 
 if __name__ == "__main__":
     manager.run()
