71d26a5f22157af51a26f87a6e5db8c486fc5298
Aaron Trujillo <atrujilloembry@gmail.com>
2021-05-05 20:47:24 -0400

    volviendo cambios

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_cors import CORS
-from models import db, Services, Profile, Communes, User
+from models import db, Services, Profile, Communes, Availability, Ratings, User, Requests
 from flask_bcrypt import Bcrypt
+from datetime import date, datetime, time
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASEDIR, 'test.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASEDIR, 'te_ayudo.db')
 app.config["DEBUG"] = True
 app.config["ENV"] = "development"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -32,104 +33,12 @@ bcrypt = Bcrypt(app)
 def main():
     return render_template('index.html')
 
-#Cesar inicio
-#Crear un nuevo usuario
-@app.route("/user/register", methods=["POST"])
-def get_user():
-    user = User()
-    user.email = request.json.get("email")
-    user.rut = request.json.get("rut")
-    password_hash = bcrypt.generate_password_hash(request.json.get('password'))
-    user.password = password_hash
-
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(user.serialize_all_fields()), 200
-
-@app.route("/user/login", methods=["POST"])
-def login():
-    email = request.json.get("email")
-    password = request.json.get("password")
-
-    #valida que el usario exista
-    user = User.query.filter_by(email=email).first()
-
-    if user is None:
-        return jsonify("This user doesn't exist"), 404
-
-    if bcrypt.check_password_hash(user.password, password): #retorna booleano
-        access_token =create_access_token(identity=email)
-        return jsonify({
-            "user": user.serialize_all_fields(),
-            "access_token": access_token
-        })
-
-@app.route('/user/profile/<int:id>', methods=['PUT'])
-def get_profile_id(id):
-    if request.method == 'PUT':
-        if id is not None:
-            profile = Profile.query.filter_by(id=id).first()
-            if profile is None :
-                return jsonify("This user doesn't exist"), 200
-            user = User.query.filter_by(id=profile.user_id).first()
-            password_hash = bcrypt.generate_password_hash(request.json.get('password'))
-            user.password = password_hash
-            profile.address = request.qjson.get("address")
-            profile.id_profile = request.json.get("id_profile")
-            profile.role = request.json.get("role")
-            profile.phone = request.json.get("phone")
-            profile.address = request.json.get("address")
-            profile.question = request.json.get("question")
-            profile.answer = request.json.get("answer")
-            profile.id_commune = request.json.get("id_commune")
-
-            db.session.commit()
-            return jsonify("Profile updated"), 200
-@app.route('/user/profile', methods=["GET", "POST"])
-def get_profile():
-    if request.method == "POST":
-        user = User()
-        user.email = request.json.get("email")
-        user.rut = request.json.get("rut")
-        user.full_name = request.json.get("full_name")
-        user.last_name = request.json.get("last_name")
-        user.phone = request.json.get("phone")
-        user.address = request.json.get("address")
-        user.id_commune = request.json.get("id_commune")
-        password_hash = bcrypt.generate_password_hash(request.json.get('password'))
-        user.password = password_hash
-
-        db.session.add(user)
-        db.session.commit()
-
-        profile = Profile()
-        profile.role = request.json.get("role")
-        profile.question = request.json.get("question")
-        profile.answer = request.json.get("answer")
-       
-        if profile.role != "client":    
-            profile.attention_communes = request.json.get("attention_communes")
-            profile.experience = request.json.get("experience")
-
-        db.session.add(profile)
-        db.session.commit()
-        return jsonify({
-            'user':user.serialize_all_fields(),
-            'profile':profile.serialize_all_fields()
-            }), 200
-
-    if request.method == "GET":
-        profiles = Profile.query.all()
-        profiles = list(map(lambda profile: profile.serialize_strict(), profiles))
-        return jsonify(profiles), 200
-#Cesar fin
-
 @app.route("/service", methods=["GET", "POST"])
 def get_services():
     if request.method == "POST":
         service = Services()
-        service.Id_Service = request.json.get("Id_Service")
-        service.Name_Service = request.json.get("Name_Service")
+        service.id_service = request.json.get("id_service")
+        service.name_service = request.json.get("name_service")
         db.session.add(service)
         db.session.commit()
         return jsonify(service.serialize_all_fields()), 200
@@ -139,13 +48,38 @@ def get_services():
         services = list(map(lambda service: service.serialize_strict(), services))
         return jsonify(services), 200
 
+@app.route("/profile", methods=["GET", "POST"])
+def get_profile():
+    if request.method == "POST":
+        profile = Profile()
+        profile.id_profile = request.json.get("id_profile")
+        profile.role = request.json.get("role")
+        profile.full_name = request.json.get("full_name")
+        profile.last_name = request.json.get("last_name")
+        profile.phone = request.json.get("phone")
+        profile.address = request.json.get("address")
+        profile.question = request.json.get("question")
+        profile.answer = request.json.get("answer")
+        profile.id_commune = request.json.get("id_commune")
+        profile.attention_communes = request.json.get("attention_communes")
+        profile.experience = request.json.get("experience")
+
+        db.session.add(profile)
+        db.session.commit()
+        return jsonify(profile.serialize_all_fields()), 200
+
+    if request.method == "GET":
+        profiles = Profile.query.all()
+        profiles = list(map(lambda profile: profile.serialize_strict(), profiles))
+        return jsonify(profiles), 200
+
 @app.route("/communes", methods=["GET", "POST"])
 def get_communes():
     if request.method == "POST":
         communes = Communes()
-        communes.Id_Region = request.json.get("Id_Region")
-        communes.Id_Commune = request.json.get("Id_Commune")
-        communes.Name_Commune = request.json.get("Name_Commune")
+        communes.name_region = request.json.get("name_region")
+        communes.id_commune = request.json.get("id_commune")
+        communes.name_commune = request.json.get("name_commune")
         db.session.add(communes)
         db.session.commit()
         return jsonify(communes.serialize_all_fields()), 200
@@ -154,6 +88,84 @@ def get_communes():
         communes = Communes.query.all()
         communes = list(map(lambda commune: commune.serialize_strict(), communes))
         return jsonify(communes), 200
+
+@app.route("/availability", methods=["GET", "POST"])
+def get_availability():
+    if request.method == "POST":
+        availability = Availability()
+        availability.id_profile = request.json.get("id_profile")             
+        availability.date = request.json.get("date")
+        availability.hour = request.json.get("hour")
+        db.session.add(availability)
+        db.session.commit()
+        return jsonify(availability.serialize_all_fields()), 200
+
+    if request.method == "GET":
+        availabilitys = Availability.query.all()
+        availabilitys = list(map(lambda availability: availability.serialize_strict(), availabilitys))
+        return jsonify(availabilitys), 200
+
+@app.route("/ratings", methods=["GET", "POST"])
+def get_ratings():
+    if request.method == "POST":
+        rating = Ratings()
+        rating.id_profile = request.json.get("id_profile")             
+        rating.rating = request.json.get("rating")
+        db.session.add(rating)
+        db.session.commit()
+        return jsonify(rating.serialize_all_fields()), 200
+
+    if request.method == "GET":
+        ratings = Ratings.query.all()
+        ratings = list(map(lambda rating: rating.serialize_strict(), ratings))
+        return jsonify(ratings), 200
+
+@app.route("/user", methods=["GET", "POST"])
+def get_user():
+    if request.method == "POST":
+        user = User()
+        user.id_user = request.json.get("id_user")             
+        user.rut = request.json.get("rut")
+        user.email = request.json.get("email")             
+        password_hash = bcrypt.generate_password_hash(request.json.get('password'))
+        user.password = password_hash
+        password_hash_prev = bcrypt.generate_password_hash(request.json.get('password_previous'))
+        user.password_previous = password_hash_prev     
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(user.serialize_all_fields()), 200
+
+    if request.method == "GET":
+        users = User.query.all()
+        users = list(map(lambda user: user.serialize_strict(), users))
+        return jsonify(users), 200
+
+@app.route("/requests", methods=["GET", "POST"])
+def get_requests():
+    if request.method == "POST":
+        requests = Requests()
+        requests.id_request = request.json.get("id_request")
+        requests.id_service = request.json.get("id_service")
+        requests.id_profile = request.json.get("id_profile")
+        requests.id_commune = request.json.get("id_commune")
+        requests.request_status = request.json.get("request_status")
+        requests.full_name = request.json.get("full_name")
+        requests.last_name = request.json.get("last_name")
+        requests.contact_phone = request.json.get("contact_phone")
+        requests.address = request.json.get("address")
+        requests.rating = request.json.get("rating")
+        requests.date = request.json.get("date")
+        requests.hour = request.json.get("hour")
+
+        db.session.add(requests)
+        db.session.commit()
+        return jsonify(requests.serialize_all_fields()), 200
+
+    if request.method == "GET":
+        requests = Requests.query.all()
+        requests = list(map(lambda request: request.serialize_strict(), requests))
+        return jsonify(requests), 200
+
 
 if __name__ == "__main__":
     manager.run()
--- models.py
+++ models.py
@@ -1,188 +1,121 @@
 from flask_sqlalchemy import SQLAlchemy
 
+from datetime import datetime
+
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    rut = db.Column(db.String(10), unique=True)
-    email = db.Column(db.String(30), nullable=False, unique=True)
-    password = db.Column(db.String(10), nullable=False)
-    full_name = db.Column(db.String(60), nullable=False)
-    last_name = db.Column(db.String(90), nullable=False)
-    phone = db.Column(db.Integer, nullable=False)
-    address = db.Column(db.String(150), nullable=False)
-    id_commune = db.Column (db.Integer, db.ForeignKey("communes.id", ondelete='CASCADE'), nullable=True)
-    profiles = db.relationship('Profile', backref='user', cascade='all, delete', lazy=True) #uselist=False si la relacion es uno a uno
-    
-    def _repr_(self):
-        return "<User %r>" % self.email
-
-    def serialize_all_fields(self):
-        return {
-            "id": self.id,
-            "email":self.email,
-            "rut": self.rut,
-            "full_name": self.full_name,
-            "last_name": self.last_name,
-            "phone":self.phone,  
-            "address": self.address,
-            "id_commune": self.id_commune
-        }
-
-    def serialize_strict(self):
-        return {
-            "id": self.id,
-            "username":self.email
-        }
-
-class Profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    role = db.Column(db.String(15), nullable=False)
-    question = db.Column(db.String(100), nullable=True)
-    answer = db.Column(db.String(200), nullable=True)
-    attention_communes = db.Column(db.String(200), nullable=True)
-    experience = db.Column(db.String(200), nullable=True)
-    id_user = db.Column (db.Integer, db.ForeignKey("user.id", ondelete='CASCADE'), nullable=True)
-    ratings = db.relationship('Ratings', backref='profile', cascade='all, delete', lazy=True) 
-    availabilities = db.relationship('Availability', backref='profile', cascade='all, delete', lazy=True) 
-    requests = db.relationship('Requests', backref='profile', cascade='all, delete', lazy=True) 
-
-    def __repr__(self):
-        return "<Profile %r>" % self.role
-
-    def serialize_all_fields(self):
-        return {
-        "id": self.id, 
-        "role": self.role,
-        "question":self.question,  
-        "answer":self.answer,
-        "attention_communes":self.attention_communes,  
-        "experience": self.experience                              
-        }
-
-    def serialize_strict(self):
-        return {
-        "id_profile": self.id,
-        "role": self.role
-        }
-
-class Availability(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String(10), nullable=False) #default=db.func.current_timestamp())
-    hour = db.Column(db.String(10), nullable=False)
-    id_profile = db.Column (db.Integer, db.ForeignKey("profile.id", ondelete='CASCADE'), nullable=True)
-
-    def __repr__(self):
-        return "<Availability %r>" % self.id
-
-    def serialize_all_fields(self):
-        return {
-        "id": self.id,                
-        "date": self.date,
-        "hour":self.hour  
-        }
-
-    def serialize_strict(self):
-        return {
-        "id": self.id,
-        "date": self.date
-        }
-
-class Requests(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    id_service = db.Column(db.Integer, nullable=False)
-    id_commune = db.Column(db.Integer, nullable=False)
-    request_status = db.Column(db.String(10), nullable=False)
-    full_name = db.Column(db.String(60), nullable=False)
-    last_name = db.Column(db.String(90), nullable=False)
-    contact_phone = db.Column(db.Integer, nullable=False)
-    address = db.Column(db.String(150), nullable=False)
-    date = db.Column(db.String(10), nullable=False)
-    hour = db.Column(db.String(10), nullable=False)
-    id_profile = db.Column (db.Integer, db.ForeignKey("profile.id", ondelete='CASCADE'), nullable=True)
-    id_service = db.Column (db.Integer, db.ForeignKey("services.id", ondelete='CASCADE'), nullable=True)
-
-    def __repr__(self):
-        return "<Requests %r>" % self.request_status
-
-    def serialize_all_fields(self):
-        return {
-        "id": self.id,
-        "id_service": self.id_service,
-        "id_profile": self.id_profile,
-        "id_commune": self.id_commune,
-        "request_status": self.request_status,
-        "full_name":self.full_name,  
-        "last_name": self.last_name,
-        "contact_phone":self.contact_phone,  
-        "address": self.address,  
-        "date":self.date,
-        "hour": self.hour                             
-        }
-
-    def serialize_strict(self):
-        return {
-        "id": self.id,  
-        "id_service": self.id_service,
-        "id_profile": self.id_profile,  
-        "id_commune": self.id_commune,
-        "request_status": self.request_status,
-        }
-
 class Services(db.Model):
-    id= db.Column(db.Integer, primary_key=True)
+    id_service = db.Column(db.Integer, primary_key=True)
     name_service = db.Column(db.String(50), nullable=False)
-    requests = db.relationship('Requests', backref='services', cascade='all, delete', lazy=True) 
 
     def __repr__(self):
         return "<Services %r>" % self.name_service
 
     def serialize_all_fields(self):
         return {
-        "id": self.id,
+        "id_service": self.id_service,
         "name_service":self.name_service  
         }
 
     def serialize_strict(self):
         return {
-        "id": self.id,
+        "id_service": self.id_service,
         "name_service":self.name_service
         }
 
-class Communes(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name_region = db.Column(db.String(100), nullable=False)
-    name_commune = db.Column(db.String(150), nullable=False)
-    user = db.relationship('User', backref='communes', cascade='all, delete', uselist=False) 
+
+class Profile(db.Model):
+    id_profile = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(15), nullable=False)
+    full_name = db.Column(db.String(60), nullable=False)
+    last_name = db.Column(db.String(90), nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String(150), nullable=False)
+    question = db.Column(db.String(100), nullable=True)
+    answer = db.Column(db.String(200), nullable=True)
+    id_commune = db.Column(db.Integer, primary_key=True)
+    attention_communes = db.Column(db.String(200), nullable=True)
+    experience = db.Column(db.String(200), nullable=True)
+
 
     def __repr__(self):
-        return "<Communes %r>" % self.name_region
+        return "<Profile %r>" % self.role
 
     def serialize_all_fields(self):
         return {
-        "id":self.id,
+        "id_profile": self.id_profile,
+        "role": self.role,
+        "full_name":self.full_name,  
+        "last_name": self.last_name,
+        "phone":self.phone,  
+        "address": self.address,
+        "question":self.question,  
+        "answer":self.answer,
+        "id_commune": self.id_commune,
+        "attention_communes":self.attention_communes,  
+        "experience": self.experience                              
+        }
+
+    def serialize_strict(self):
+        return {
+        "id_profile": self.id_profile,  
+        "role": self.role
+        }
+
+
+class Communes(db.Model):
+    name_region = db.Column(db.String(100), nullable=False)
+    id_commune = db.Column(db.Integer, primary_key=True)
+    name_commune = db.Column(db.String(150), nullable=False)
+
+    def __repr__(self):
+        return "<Communes %r>" % self.id_commune
+
+    def serialize_all_fields(self):
+        return {
         "name_region": self.name_region,
+        "id_commune":self.id_commune,
         "name_commune":self.name_commune  
         }
 
     def serialize_strict(self):
         return {
-        "id":self.id,
-        "name_region": self.name_region
+        "name_region": self.name_region,
+        "id_commune":self.id_commune
         }
 
+
+class Availability(db.Model):
+    id_profile = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10), nullable=False) #default=db.func.current_timestamp())
+    hour = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return "<Availability %r>" % self.id_profile
+
+    def serialize_all_fields(self):
+        return {
+        "id_profile": self.id_profile,                
+        "date": self.date,
+        "hour":self.hour  
+        }
+
+    def serialize_strict(self):
+        return {
+        "id_profile": self.id_profile,
+        "date": self.date
+        }
+
+
 class Ratings(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
     id_profile = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer, nullable=False)
-    profile_id = db.Column (db.Integer, db.ForeignKey("profile.id", ondelete='CASCADE'), nullable=True)
 
     def __repr__(self):
         return "<Ratings %r>" % self.id_profile
 
     def serialize_all_fields(self):
         return {
-        "id":self.id,
         "id_profile": self.id_profile,                
         "rating": self.rating 
         }
@@ -191,3 +124,70 @@ class Ratings(db.Model):
         return {
         "id_profile": self.id_profile
         }
+
+
+class User(db.Model):    
+    id_user = db.Column(db.Integer, primary_key=True)
+    rut = db.Column(db.String(9), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    password_previous = db.Column(db.String(50), nullable=True)    
+
+    def __repr__(self):
+        return "<User %r>" % self.id_user
+
+    def serialize_all_fields(self):
+        return {
+        "id_user": self.id_user,
+        "rut":self.rut,
+        "email":self.email         
+        }
+
+    def serialize_strict(self):
+        return {
+        "id_user": self.id_user,
+        "rut":self.rut
+        }
+
+
+class Requests(db.Model):
+    id_request = db.Column(db.Integer, primary_key=True)
+    id_service = db.Column(db.Integer, nullable=False)
+    id_profile = db.Column(db.Integer, nullable=False)
+    id_commune = db.Column(db.Integer, nullable=False)
+    request_status = db.Column(db.String(10), nullable=False)
+    full_name = db.Column(db.String(60), nullable=False)
+    last_name = db.Column(db.String(90), nullable=False)
+    contact_phone = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String(150), nullable=False)
+    rating = db.Column(db.Integer, nullable=True)
+    date = db.Column(db.String(10), nullable=False)
+    hour = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return "<Requests %r>" % self.id_request
+
+    def serialize_all_fields(self):
+        return {
+        "id_request": self.id_request,
+        "id_service": self.id_service,
+        "id_profile": self.id_profile,
+        "id_commune": self.id_commune,
+        "request_status": self.request_status,
+        "full_name":self.full_name,  
+        "last_name": self.last_name,
+        "contact_phone":self.contact_phone,  
+        "address": self.address,
+        "rating":self.rating,  
+        "date":self.date,
+        "hour": self.hour                             
+        }
+
+    def serialize_strict(self):
+        return {
+        "id_request": self.id_request,  
+        "id_service": self.id_service,
+        "id_profile": self.id_profile,  
+        "id_commune": self.id_commune
+        }
+
